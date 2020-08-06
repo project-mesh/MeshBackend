ddae94b4b845e4e04c348cb2f2951f97e8c53ff6
geofake37 <740950342@qq.com>
2020-08-06 15:08:43 +0800

    modify the HelloWorld controller

--- Controllers/HelloWorld.cs
+++ Controllers/HelloWorld.cs
@@ -4,28 +4,38 @@ using Microsoft.Extensions.Logging;
 namespace MeshBackend.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("HelloWorld")]
     [Produces("application/json")]
-    public class PostController:Controller
+    public class HelloWorld:Controller
     {
-        private readonly ILogger<GetController> _logger;
+        private readonly ILogger<HelloWorld> _logger;
 
-        public PostController(ILogger<GetController> logger)
+        public HelloWorld(ILogger<HelloWorld> logger)
         {
             _logger = logger;
         }
-        
+
         public class Result
         {
             public string Description { get; set; }
         }
         
         [HttpPost]
-        public JsonResult Post(string text)
+        public JsonResult Post1599655068(string text)
         {
             var result = new Result()
             {
                 Description = text
+            };
+            return Json(result);
+        }
+        
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var result = new Result()
+            {
+                Description = "Project Mesh Web API"
             };
             return Json(result);
         }
